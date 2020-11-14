42bc679cb7a5711537de25448bcf5b5a93baeebe
angelhun-afk <74453605+angelhun-afk@users.noreply.github.com>
2020-11-14 12:20:38 +0100

    HTbomb. sh

--- TBomb.sh
+++ TBomb.sh
@@ -23,37 +23,30 @@ pause() {
 }
 banner() {
     clear
-    echo -e "\e[1;31m"
+    echo -e "\e[1;36m"
     if ! [ -x "$(command -v figlet)" ]; then
         echo 'Introducing TBomb'
     else
         figlet TBomb
     fi
     if ! [ -x "$(command -v toilet)" ]; then
-        echo -e "\e[4;34m This Bomber Was Created By \e[1;32mSpeedX \e[0m"
+        echo -e "\e[4;36m This Bomber Was Created By \e[1;36mSpeedX \e[0m"
     else
-        echo -e "\e[1;34mCreated By \e[1;34m"
+        echo -e "\e[1;36mCreated By \e[1;34m"
         toilet -f mono12 -F border SpeedX
     fi
-    echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
-    echo -e "\e[1;32m           Mail: ggspeedx29@gmail.com \e[0m"
-    echo -e "\e[4;32m   YouTube: https://www.youtube.com/c/GyanaTech \e[0m"
+    echo -e "\e[1;36m For Any Queries Mail Me!!!\e[0m"
+    echo -e "\e[1;36m           Mail: ggspeedx29@gmail.com \e[0m"
+    echo -e "\e[4;36m   YouTube: https://www.youtube.com/c/GyanaTech \e[0m"
     echo " "
 
 }
 
 init_environ(){
     declare -A backends; backends=(
-        ["arch"]="pacman -S --noconfirm"
-        ["debian"]="apt-get -y install"
-        ["ubuntu"]="apt -y install"
+ 
         ["termux"]="apt -y install"
-        ["fedora"]="yum -y install"
-        ["redhat"]="yum -y install"
-        ["SuSE"]="zypper -n install"
-        ["sles"]="zypper -n install"
-        ["darwin"]="brew install"
-        ["alpine"]="apk add"
+        
     )
 
     INSTALL="${backends[$distro]}"
@@ -62,7 +55,7 @@ init_environ(){
         PYTHON="python"
         SUDO=""
     else
-        PYTHON="python3"
+        PYTHON="python2"
         SUDO="sudo"
     fi
     PIP="$PYTHON -m pip"
@@ -78,7 +71,7 @@ install_deps(){
         $PIP install -r requirements.txt
     else
         echo "We could not install dependencies."
-        echo "Please make sure you have git, python3, pip3 and requirements installed."
+        echo "Please make sure you have git, python2, pip2 and requirements installed."
         echo "Then you can execute bomber.py ."
         exit
     fi
@@ -102,7 +95,7 @@ fi
 while :
 do
     banner
-    echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
+    echo -e "\e[4;36m Please Read Instruction Carefully !!! \e[0m"
     echo " "
     echo "Press 1 To  Start SMS  Bomber "
     echo "Press 2 To  Start CALL Bomber "
@@ -121,17 +114,17 @@ do
         $PYTHON bomber.py --mail
         exit
     elif [ $ch -eq 4 ];then
-        echo -e "\e[1;34m Downloading Latest Files..."
+        echo -e "\e[1;36m Downloading Latest Files..."
         rm -f .update
         $PYTHON bomber.py --update
-        echo -e "\e[1;34m RUN TBomb Again..."
+        echo -e "\e[1;36m RUN TBomb Again..."
         pause
         exit
     elif [ $ch -eq 5 ];then
         banner
         exit
     else
-        echo -e "\e[4;32m Invalid Input !!! \e[0m"
+        echo -e "\e[4;36m Invalid Input !!! \e[0m"
         pause
     fi
 done
